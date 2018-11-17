f8f225ee65f580cacc34a8ef29001d7cfd0446bf
Noé Fernandes <noepessoa@outlook.com>
2018-11-17 16:14:12 -0300

    Primeiro commit

---
+++ dev_canvas/src/canvas.cpp
@@ -0,0 +1,93 @@
+/*!
+ * Canvas class implementation.
+ * @file canvas.cpp
+ */
+
+#include <stdexcept>
+#include <iostream>
+
+#include "../include/canvas.h"
+
+namespace canvas {
+
+    /*!
+     * Deep copy of the canvas.
+     * @param clone The object we are copying from.
+     */
+    Canvas::Canvas( const Canvas & clone )
+    {
+        // TODO
+    }
+
+
+    /*!
+     * @param source The object we are copying information from.
+     * @return A reference to the `this` object.
+     */
+    Canvas& Canvas::operator=( const Canvas & source )
+    {
+        // TODO: incompleto
+
+        return *this;
+    }
+
+    void Canvas::clear( const Color& color )
+    {
+        // TODO
+    }
+
+    /*!
+     * @throw `std::invalid_argument()` it the pixel coordinate is located outside the canvas.
+     * @param p The 2D coordinate of the pixel we want to know the color of.
+     * @return The pixel color.
+     */
+    Color Canvas::pixel( coord_type x, coord_type y ) const
+    {
+        // TODO
+    }
+
+    /*!
+     * @note Nothing is done if the  pixel coordinate is located outside the canvas.
+     * @param p The 2D coordinate of the pixel we want to change the color.
+     * @param c The color.
+     */
+    void Canvas::pixel( coord_type x, coord_type y, const Color& c )
+    {
+        // TODO
+    }
+
+
+    /*!
+     * Draws on the canvas a horizontal line.
+     * @param p The 2D coordinate of the initial pixel of the line.
+     * @param length The horizontal length of the line in pixels.
+     */
+    void Canvas::hline( coord_type x, coord_type y, size_t length, const Color& color )
+    {
+        // TODO
+    }
+
+    /*!
+     * Draws on the canvas a vertical line.
+     * @param p The 2D coordinate of the initial pixel of the line.
+     * @param length The vertical length of the line in pixels.
+     */
+    void Canvas::vline( coord_type x, coord_type y, size_t length, const Color& color )
+    {
+        // TODO
+    }
+
+    /*!
+     * Draws on the canvas a filled box. The origin of the box is the top-left corner.
+     * The box is compose of horizontal lines, drawn top to bottom.
+     * @param p The 2D coordinate of the initial pixel of the box (top-left corner).
+     * @param width The box's width in pixels.
+     * @param height The box's heigth in pixels.
+     */
+    void Canvas::box( coord_type x, coord_type y, size_t width, size_t height , const Color& color)
+    {
+        // TODO
+    }
+}
+
+//================================[ canvas.cpp ]================================//
